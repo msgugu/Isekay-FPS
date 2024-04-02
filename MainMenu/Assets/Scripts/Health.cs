ca45175ad16fd70a890d65e838cbf065c92bdea3
YuseokY <doqldoql12@gmail.com>
2024-04-02 11:57:08 +0900

    0
    0

--- MainMenu/Assets/Scripts/Health.cs
+++ MainMenu/Assets/Scripts/Health.cs
@@ -8,6 +8,8 @@ public class Health : MonoBehaviour, IDamageable
 {
     // UI 
     public Test_UIProgressBar healthProgressBar;
+    public GameObject HealImage; // 회복하면 나오는 UI Image
+    public GameObject BloodImage; // 체력이 없으면 나오는 UI Image
 
     const float maxHealth = 100f;
     float currentHealth = maxHealth;
@@ -18,7 +20,14 @@ public class Health : MonoBehaviour, IDamageable
     {
         PV = GetComponent<PhotonView>();
         playerManager = PhotonView.Find((int)PV.InstantiationData[0]).GetComponent<PlayerManager>();
+        //UpdateHealthBar();
+    }
+
+    void Start()
+    {
         UpdateHealthBar();
+        HealImage.SetActive(false);
+        BloodImage.SetActive(false);
     }
 
     public void TakeDamage(float damage)
@@ -32,9 +41,9 @@ public class Health : MonoBehaviour, IDamageable
 
         currentHealth -= damage;
         UpdateHealthBar();
+        CheckHealStatus(); // 체력 체크
 
         //healthbarImage.fillAmount = currentHealth / maxHealth;
-
         if (currentHealth <= 0)
         {
             playerManager.CreateKillLog(info.Sender.NickName, PhotonNetwork.NickName);
@@ -53,7 +62,6 @@ public class Health : MonoBehaviour, IDamageable
             currentHealth = maxHealth;
         }
 
-
         PV.RPC(nameof(RPC_TakeHeal), PV.Owner, currentHealth); // 메소드 이름을 올바르게 수정
     }
 
@@ -62,6 +70,8 @@ public class Health : MonoBehaviour, IDamageable
     {
         currentHealth = hp;
         UpdateHealthBar();
+        ShowHealImage();
+        CheckHealStatus(); // 체력 체크
         Debug.Log(currentHealth);
     }
 
@@ -78,6 +88,39 @@ public class Health : MonoBehaviour, IDamageable
             float healthPercent = currentHealth / maxHealth;
             healthProgressBar.SetFillAmount(healthPercent);
         }
+    }
+
+    /// <summary>
+    /// 체력 체크 하기
+    /// </summary>
+    void CheckHealStatus()
+    {
+        if (PV.IsMine)
+        {
+            if(currentHealth <= maxHealth * 0.3f)
+            {
+                BloodImage.SetActive(true);
+            }
+            else
+            {
+                BloodImage.SetActive(false);
+            }
+        }
+    }
+
+    void ShowHealImage()
+    {
+        if (PV.IsMine)
+        {
+            HealImage.SetActive(true);
+            StartCoroutine(HideHealImageAfterTime(1f));
+        }
+    }
+
+    IEnumerator HideHealImageAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        HealImage.SetActive(false);
     }
 
 }
